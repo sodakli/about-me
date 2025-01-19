175a9509f543d6f808f2b6a646ae7dd8bd4ea5c1
Soda <sodaklicomm@gmail.com>
2025-01-19 17:06:08 -0300

    almost finished version of the website

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Sodakli
-It's an about-me based site, which i developed for the purpose of showing some of my social informations and also my skills. 
+I made an about-me based site, which i developed for the purpose of showing some of my social informations and also my skills. 
 It was completely based on ayo.so. 
---
+++ src/script.js
@@ -0,0 +1,27 @@
+document.getElementById('copyButton').addEventListener('click', function() {
+
+    const discordInvite = 'https://discord.gg/your-server-code';
+
+
+    const tempInput = document.createElement('input');
+    tempInput.value = discordInvite;
+
+  
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+
+    // Execute the copy command
+    document.execCommand('copy');
+
+ 
+    document.body.removeChild(tempInput);
+
+
+    const button = document.getElementById('copyButton');
+    button.textContent = 'Copied to clipboard';
+    
+    setTimeout(function() {
+        button.textContent = 'Join Revolt';
+    }, 2000);
+});
